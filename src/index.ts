93f3057b0c66c7fe7b6d66c91073a98be2e4b759
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-07-01 01:44:44 +0530

    index - middlewares added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express, { Request, Response } from "express";
 import { ENV } from "./configs/env";
 import { connectDB } from "./configs/db";
+import cors from 'cors';
+import { clerkMiddleware } from "@clerk/express";
 
 
 const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
@@ -13,7 +18,7 @@ app.get("/", (req: Request, res: Response) => {
 
 const PORT = ENV.PORT;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on: http://localhost:${PORT}`)
-  connectDB();
+  await connectDB();
 })
