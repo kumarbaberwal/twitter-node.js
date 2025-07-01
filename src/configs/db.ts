ab99fe8d15e61357b2f756c56bad75a19b6a46ef
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-07-02 01:46:18 +0530

    db modified

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -4,7 +4,7 @@ import { ENV } from "./env"
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(ENV.MONGO_URI)
-    console.log("Connected Successfully: ", conn.connection.host)
+    console.log("DB Connected Successfully: ", conn.connection.host)
   } catch (error) {
     console.log("Error connecting to mongoDB", error)
     process.exit(1)
